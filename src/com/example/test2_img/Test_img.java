2011f16cfb6f43a28ab0d9f27526bf5be93e5a36
vul9156 <vul9156@hotmail.com>
2014-05-14 03:13:41 +0800

    can display photo & word

--- src/com/example/test2_img/Test_img.java
+++ src/com/example/test2_img/Test_img.java
@@ -18,16 +18,26 @@ import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
 import android.widget.TableRow;
 import android.widget.TextView;
+
 public class Test_img extends Activity { 
+	
 	/** Called when the activity is first created. */
 	private int index = 0; 
 	private TableRow tupian;
 	private TableRow shanchu; 
-	@Override public void onCreate(Bundle savedInstanceState) {
+	private TableRow word; 
+	 public String UserID = "123123";
+	
+	@Override 
+	public void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState); 
 		setContentView(R.layout.activity_test_img);
+		
 		Button button = (Button)findViewById(R.id.b01); 
+		
 		button.setText("选择图片");
+		
 		button.setOnClickListener(new Button.OnClickListener(){ 
 			@Override public void onClick(View v) { 
 				Intent intent = new Intent(); 
@@ -37,32 +47,74 @@ public class Test_img extends Activity {
 				intent.setAction(Intent.ACTION_GET_CONTENT);
 				/* 取得相片后返回本画面 */
 				startActivityForResult(intent, 1); } }); }
-	@Override protected void onActivityResult(int requestCode, int resultCode, Intent data) { 
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) { 
+		
 		if (resultCode == RESULT_OK) { 
+		
 			Uri uri = data.getData(); Log.e("uri", uri.toString()); 
+		
 			ContentResolver cr = this.getContentResolver();
-			try { Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri)); // String str = "iv0"+Integer.toString(aa);
+			
+			try { 
+				Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+				// String str = "iv0"+Integer.toString(aa);
+			
 			// ImageView imageView = (ImageView) findViewById(R.id.);
 			tupian = (TableRow) findViewById(R.id.tupian); 
+			word=(TableRow)findViewById(R.id.word); 
 			shanchu = (TableRow) findViewById(R.id.shanchu); 
+			
 			//tupian.addView(AddImageView(), 1); 
-			tupian.addView(AddImageView(), 100 , 100);
-			shanchu.addView(AddButton(), 100 , 50); 
+			
+			tupian.addView(AddImageView(), 300 , 300);
+			word.addView(ShowWord(),300,300);
+			shanchu.addView(AddButton(), 300 , 50); 
+			
 			ImageView imageView1 = (ImageView) findViewById(index);
-			ImageView(imageView1 , bitmap ); } 
+			
+			ImageView_bi(imageView1 , bitmap );
+			
+//			TextView textview1 = (TextView) findViewById(0);
+			
+//			textview1.setText("Example TextView by corn");
+			} 
+			
 			catch (FileNotFoundException e) { 
+			
 				Log.e("Exception", e.getMessage(),e); } } 
-		super.onActivityResult(requestCode, resultCode, data); } 
+		
+			super.onActivityResult(requestCode, resultCode, data); } 
+	
 	// 动态添加 图片
-	private void ImageView(ImageView imageView , Bitmap bitmap ) { 
-		Bitmap mBitmap = Bitmap.createScaledBitmap(bitmap, 50, 50, true); 
+	
+	private void ImageView_bi(ImageView imageView , Bitmap bitmap ) { 
+	
+		Bitmap mBitmap = Bitmap.createScaledBitmap(bitmap,100,100, true); 
 		imageView.setImageBitmap(mBitmap); } 
+
+	
 	protected View AddImageView() {
-		index++; ImageView btn = new ImageView(this); 
+		
+		index++; 
+		ImageView btn = new ImageView(this); 
 		btn.setId(index);
 		btn.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-		// btn.setScaleType(FIT_XY); 
 		return btn; }
+
+	protected View ShowWord() {
+	//	index++;
+		// TODO Auto-generated method stub
+		TextView btn=new TextView(this);
+		btn.setId(index);
+		btn.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		btn.setText(UserID); //這一行是可以顯示文字的
+		return btn;
+		
+		
+	}
+
 protected View AddButton() { 
 	// index1++; 
 	Button btn = new Button(this);
@@ -73,7 +125,8 @@ protected View AddButton() {
 			int i = v.getId() ; 
 			shanchu.removeView(v) ;
 			tupian.removeView(tupian.findViewById(i)) ; } });
-	btn.setText("删除"); return btn; } 
+	btn.setText("删除");
+	return btn; } 
 			
 
 	}
